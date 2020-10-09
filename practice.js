a04179e61313cf84d39876f36d0d6410ad1dcc2d
thibaudszy <thivaudszy@gmail.com>
2020-10-09 10:58:23 +0200

    added a server response

--- practice.js
+++ practice.js
@@ -7,6 +7,26 @@ const app = express();
 // defining the port that will be used
 const port = 3000;
 
-//start listerning to the port defined above
-app.listen(port);
+//function to display a message in the terminal when port is open
+function onListen() {
+    console.log(`listerning to port ${port}`)
+}
 
+//start listerning to the port defined above
+app.listen(port,onListen());
+
+
+//handling the request
+app.get (
+    '/json', //path
+    (request,response) => { //handler callback
+        console.log('request received');
+        //defining the json object to send
+        const myAwesomeResponse = {
+            answer1: 'hey!',
+            answer2: 'you'
+        }
+        response.send(myAwesomeResponse);
+
+    }
+)
