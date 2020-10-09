89a7fd3986a4a6c36b94125e02b3b5aee89138c9
thibaudszy <thivaudszy@gmail.com>
2020-10-09 10:24:52 +0200

    'initialisation'

---
+++ practice.js
@@ -0,0 +1,12 @@
+//importing the express library
+const express = require('express');
+
+//creating an express server
+const app = express();
+
+// defining the port that will be used
+const port = 3000;
+
+//start listerning to the port defined above
+app.listen(port);
+
